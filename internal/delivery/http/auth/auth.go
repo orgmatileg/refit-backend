30da177755c59de24d669880ae0489210975fed9
Luqmanul Hakim <luqmanul.hakim@grabtaxi.com>
2019-12-31 18:03:05 +0700

    fix indent docker-compose

--- internal/delivery/http/auth/auth.go
+++ internal/delivery/http/auth/auth.go
@@ -115,7 +115,7 @@ func (a auth) OAuthGoogleCallback(c echo.Context) error {
 	}
 	fmt.Println(data)
 
-	return c.Redirect(http.StatusTemporaryRedirect, "exp://192.168.43.2:19000/home")
+	return c.Redirect(http.StatusTemporaryRedirect, "exp://192.168.43.2:19000/--/home")
 
 	// GetOrCreate User in your db.
 	// Redirect or response with a token.
