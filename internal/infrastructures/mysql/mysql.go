2a7b387a21b97ca4d537e4c8aea89288a34ff23f
Luqmanul Hakim <luqmanul.hakim@grabtaxi.com>
2019-12-31 19:45:32 +0700

    fix indent docker-compose

--- internal/infrastructures/mysql/mysql.go
+++ internal/infrastructures/mysql/mysql.go
@@ -2,7 +2,9 @@ package mysql
 
 import (
 	"database/sql"
+	"refit_backend/internal/logger"
 	"time"
+
 	// mysql need this
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -22,12 +24,15 @@ func GetDB() *sql.DB {
 func createConnection() error {
 	dbCon, err := sql.Open("mysql", "root:masuk123@tcp(luqmanul.com:3306)/refit?parseTime=true")
 	if err != nil {
+		logger.Errorf("could not open mysql database dsn: %s")
 		return err
 	}
 	err = dbCon.Ping()
 	if err != nil {
+		logger.Errorf("could not ping mysql database: %s", err.Error())
 		return err
 	}
+	logger.Infof("database mysql: Connected!")
 	dbCon.SetMaxOpenConns(100)
 	dbCon.SetMaxIdleConns(10)
 	dbCon.SetConnMaxLifetime(time.Duration(300 * time.Second))
